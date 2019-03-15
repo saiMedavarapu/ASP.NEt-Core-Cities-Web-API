45189e7700219a17709a462e2da4b6fcf7fe768d
saiMedavarapu <vaibhav.medavarapu@gmail.com>
2019-03-14 22:31:45 -0700

    Resource Created

--- CityDetails/Controllers/PointsOfInterestsController.cs
+++ CityDetails/Controllers/PointsOfInterestsController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CityDetails.Models;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -20,7 +21,7 @@ namespace CityDetails.Controllers
             return Ok(city.PointsOfInterest);
         }
 
-        [HttpGet("{cityId}/pointsofinterest/{id}")]
+        [HttpGet("{cityId}/pointsofinterest/{id}", Name ="GetPointOfInterest")]
         public IActionResult GetPointOfInterest(int cityId, int id) {
             var city = CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == cityId);
             if (city == null)
@@ -35,6 +36,39 @@ namespace CityDetails.Controllers
             }
 
             return Ok(pointOfInterest);
+        }
+
+        [HttpPost("{cityId}/pointsofinterest")]
+        public IActionResult CreatePointOfInterest(int cityId, 
+            [FromBody] PointOfInterestForCreationDto pointsOfInterest)
+        {
+            if (pointsOfInterest == null)
+            {
+                return BadRequest();
+            }
+
+            var city = CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == cityId);
+            if (city == null)
+            {
+                return NotFound();
+            }
+
+            var maxPointOfInterestId = CitiesDataStore.Current.Cities.SelectMany
+                (c => c.PointsOfInterest).Max(p => p.Id);
+
+
+            var finalPointOfInterest = new PointOfInterest()
+            {
+                Id = ++maxPointOfInterestId,
+                Name = pointsOfInterest.Name,
+                Description = pointsOfInterest.Description
+
+            };
+
+            city.PointsOfInterest.Add(finalPointOfInterest);
+
+            return CreatedAtRoute("GetPointOfInterest", new
+            { cityId = cityId, Id = finalPointOfInterest.Id }, finalPointOfInterest);
         }
     }
 }
--- CityDetails/Startup.cs
+++ CityDetails/Startup.cs
@@ -2,10 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.Formatters;
 using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json.Serialization;
 
 namespace CityDetails
 {
@@ -15,7 +18,24 @@ namespace CityDetails
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc();
+            services.AddMvc()//This adds the MVC support
+                .AddMvcOptions(o => o.OutputFormatters.Add(new XmlDataContractSerializerOutputFormatter()));
+            //This lets you convert to the XML format
+              
+                
+                
+                //This lets you convert everything into Caps of first letter
+
+             /*   .AddJsonOptions(o =>
+                {
+                    if (o.SerializerSettings.ContractResolver != null)
+                    {
+                        var castedResolver = o.SerializerSettings.ContractResolver
+                        as DefaultContractResolver;
+
+                        castedResolver.NamingStrategy = null;
+                    }
+                });*/
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
